be584bf29a33d72c3bf4b37386d65b20503f1c93
Phyros <felipe_rsilva@hotmail.com>
2020-05-09 20:08:47 -0300

    commitando na ide eclipse

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Cds-Colletion
+# Cds Colletion
 This project is just personal exercise to learn. I will place some extra files as ERD and UML diagrams.
--- cd-collection/src/AppRun.java
+++ cd-collection/src/AppRun.java
@@ -3,6 +3,7 @@ public class AppRun {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Guys!");
+		System.out.println("Brinks");
 
 	}
 
